a5a9a5799fb20d25b3965fff349f1babd7ca1569
codegrann <omondihills02@gmail.com>
2024-01-24 13:09:38 +0300

    configured tailwindcss and removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="bg-black h-screen">
+      <header className="bg-pink-500">
+        <img src={logo} className="w-96" alt="logo" />
+        <p className="text-pink font-bold">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-white"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
