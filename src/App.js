884f813fb3b0cacfd9155e50f422ce18543d59dc
codegrann <omondihills02@gmail.com>
2024-01-24 13:21:12 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import logo from "./logo.svg";
-// import './App.css';
 
 function App() {
-  return (
-    <div className="bg-black h-screen">
-      <header className="bg-pink-500">
-        <img src={logo} className="w-96" alt="logo" />
-        <p className="text-pink font-bold">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-white"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="bg-gray-700 h-screen"></div>;
 }
 
 export default App;
