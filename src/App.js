9371cefda54e77ac936ae08272d66ec569bad245
codegrann <omondihills02@gmail.com>
2024-01-25 22:43:23 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from "./logo.svg";
 
 import Button from "./components/Button";
+import Io from "./components/Io";
 
 function App() {
   return (
     <div className="bg-gray-700 h-screen">
+      <Io />
       <Button label="Click me!" />
     </div>
   );
---
+++ src/components/Io.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Io() {
+  return (
+    <div>
+      <p>1234</p>
+      <input type="text" name="expression" placeholder="0" />
+    </div>
+  );
+}
+
+export default Io;
