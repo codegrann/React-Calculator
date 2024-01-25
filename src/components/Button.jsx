e69dacd83b5c36d18a828b4e760517bf8a419456
codegrann <omondihills02@gmail.com>
2024-01-25 22:37:43 +0300

    .

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({ label }) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button className="bg-blue-400 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
       {label}
     </button>
   );
