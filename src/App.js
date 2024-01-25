c898366a08cffc594ab26a734cb75d34de4a4846
codegrann <omondihills02@gmail.com>
2024-01-25 22:33:41 +0300

    created a button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+
+import Button from "./components/Button";
 
 function App() {
-  return <div className="bg-gray-700 h-screen"></div>;
+  return (
+    <div className="bg-gray-700 h-screen">
+      <Button label="Click me!" />
+    </div>
+  );
 }
 
 export default App;
